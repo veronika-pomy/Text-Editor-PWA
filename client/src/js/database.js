49493318b1411f8dd5dc7b886649b9061b2457aa
Veronika Pomyateeva <pomyateevaav@gmail.com>
2023-04-13 01:22:59 -0400

    added get and put routes to the database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,46 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Connect to the db
+  const contactDb = await openDB('jate', 1);
+
+  // New transaction with specified privilages
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Open obj store
+  const store = tx.objectStore('jate');
+
+  // Add and store content
+  const request = store.add({ content: content });
+
+  // Confirm request
+  const result = await request;
+  console.log('Saved to DB.', result);
+
+};
+
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  // Connect to the db
+  const contactDb = await openDB('jate', 1);
+
+  // New transaction with specified privilages
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Open obj store
+  const store = tx.objectStore('jate');
+
+  // Get all data in the db
+  const request = store.getAll();
+
+  // Confirm request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
