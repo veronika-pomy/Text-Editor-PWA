057d598655b897635d176c60215862a99b76a915
Veronika Pomyateeva <pomyateevaav@gmail.com>
2023-04-13 19:04:38 -0400

    added registerRoute for static assets

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,5 +26,41 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+registerRoute(
+  // callback to filter JS and CSS to cache 
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  // cache strategy, speficy storage name 
+  new CacheFirst ({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// registerRoute(
+//   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+//   new StaleWhileRevalidate({
+//     cacheName: 'asset-cache',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// );
+
 // TODO: Implement asset caching
-registerRoute( );
+  // Requirements:
+    //  Caches static assets
+    //  WHEN I register a service worker
+    // THEN I should have my static assets pre cached "upon loading" along with subsequent pages and static assets
+    // Callback example: https://developer.chrome.com/docs/workbox/modules/workbox-routing/
+    // import {registerRoute} from 'workbox-routing';
+    // registerRoute(matchCb, handlerCb);
+
+// I should have my static assets pre cached "upon loading" along with subsequent pages and static assets
+// use stale-while-revalidate only for static assets. This way your html, js, css, images etc. would be cached and quickly served to the user, but the data fetched dynamically from an API could still be fresh
+
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Text Editor
+
+Resources:
+
+https://developer.chrome.com/docs/workbox/caching-strategies-overview/#:~:text=%23%20Cache%20first%2C%20falling%20back%20to%20network&text=The%20request%20hits%20the%20cache,the%20response%20from%20the%20network.
+
+https://developer.chrome.com/docs/workbox/modules/workbox-routing/
